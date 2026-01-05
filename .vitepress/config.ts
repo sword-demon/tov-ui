ebda627f6d0a5a0e8cbf99a6c88dc413622aa1b6
王鑫123wangxin <1418667580@qq.com>
2026-01-05 23:31:31 +0800

    feat(button): 新增 Button 组件骨架
    - 添加 packages/tov-ui/src/button/index.md 组件文档
    - 更新 .vitepress/config.ts 添加组件 sidebar 配置

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Tov UI',
   description: 'This is a vue component library',
+  rewrites: {
+    // 路径映射关系
+    'docs/(.*)': '(.*)',
+    'packages/tov-ui/src/:comp/(.*)': 'components/:comp/(.*)',
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -11,6 +16,8 @@ export default defineConfig({
       { text: '介绍', link: '/introduce' },
       { text: '组件', link: '/components/' },
       { text: '工具', link: '/utils/' },
+      { text: 'Git', link: '/Git' },
+      { text: 'ESlint', link: '/ESlint' },
     ],
 
     sidebar: {
