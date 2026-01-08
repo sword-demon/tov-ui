c779ebc5322481c420ec4a1dbf9136367c1b1f9b
王鑫123wangxin <1418667580@qq.com>
2026-01-08 22:59:22 +0800

    chore(button): add utils test call in basic demo

--- packages/tov-ui/src/button/demos/basic.vue
+++ packages/tov-ui/src/button/demos/basic.vue
@@ -6,7 +6,11 @@ title: 基础使用
 这是一个`button`的基础使用的例子
 </docs>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { test } from '@tov-ui/utils'
+
+test()
+</script>
 
 <template>
   <div>
